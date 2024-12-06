ac759459368ba30b3727b2b5a149d79eb5776c55
bz <88026162+bz-hashtag-0780@users.noreply.github.com>
2024-12-06 06:41:16 +0100

    Update SendTransactionCard.tsx

--- src/components/magic/cards/SendTransactionCard.tsx
+++ src/components/magic/cards/SendTransactionCard.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable indent */
+/* eslint-disable quotes */
 import React, { useCallback, useEffect, useState } from 'react';
 import Divider from '@/components/ui/Divider';
 import { useMagic } from '../MagicProvider';
@@ -16,102 +19,119 @@ import Spacer from '@/components/ui/Spacer';
 import TransactionHistory from '@/components/ui/TransactionHistory';
 
 const SendTransaction = () => {
-  const { magic } = useMagic();
-  const [toAddress, setToAddress] = useState('');
-  const [amount, setAmount] = useState('');
-  const [disabled, setDisabled] = useState(!toAddress || !amount);
-  const [toAddressError, setToAddressError] = useState(false);
-  const [amountError, setAmountError] = useState(false);
-  const [hash, setHash] = useState('');
-  const [transactionLoading, setTransactionLoading] = useState(false);
-  const publicAddress = localStorage.getItem('user');
+	const { magic } = useMagic();
+	const [toAddress, setToAddress] = useState('');
+	const [amount, setAmount] = useState('');
+	const [disabled, setDisabled] = useState(!toAddress || !amount);
+	const [toAddressError, setToAddressError] = useState(false);
+	const [amountError, setAmountError] = useState(false);
+	const [hash, setHash] = useState('');
+	const [transactionLoading, setTransactionLoading] = useState(false);
+	const publicAddress = localStorage.getItem('user');
 
-  useEffect(() => {
-    setDisabled(!toAddress || !amount);
-    setAmountError(false);
-    setToAddressError(false);
-  }, [amount, toAddress]);
+	useEffect(() => {
+		setDisabled(!toAddress || !amount);
+		setAmountError(false);
+		setToAddressError(false);
+	}, [amount, toAddress]);
 
-  const sendTransaction = useCallback(async () => {
-    if (!magic) {
-      return;
-    }
-    try {
-      setTransactionLoading(true);
+	const sendTransaction = useCallback(async () => {
+		if (!magic) {
+			return;
+		}
+		try {
+			setTransactionLoading(true);
 
-      const response = await fcl.mutate({
-        //cadence: transaction,
-        template: 'https://flix.flow.com/v1/templates?name=transfer-flow',
-        args: (arg, t) => [arg(Number(amount).toFixed(2), t.UFix64), arg(toAddress, t.Address)],
-        proposer: magic.flow.authorization,
-        authorizations: [magic.flow.authorization],
-        payer: magic.flow.authorization,
-        limit: 999,
-      });
-      setHash(response);
-      console.log('response: ', JSON.stringify(response));
+			const response = await fcl.mutate({
+				//cadence: transaction,
+				template:
+					'https://flix.flow.com/v1/templates?name=transfer-flow',
+				args: (arg: any, t: any) => [
+					arg(Number(amount).toFixed(2), t.UFix64),
+					arg(toAddress, t.Address),
+				],
+				proposer: magic.flow.authorization,
+				authorizations: [magic.flow.authorization],
+				payer: magic.flow.authorization,
+				limit: 999,
+			});
+			setHash(response);
+			console.log('response: ', JSON.stringify(response));
 
-      console.log('Waiting for transaction to be sealed');
-      var data = await fcl.tx(response).onceSealed();
-      console.log('sealed: ', JSON.stringify(data));
+			console.log('Waiting for transaction to be sealed');
+			var data = await fcl.tx(response).onceSealed();
+			console.log('sealed: ', JSON.stringify(data));
 
-      if (data.status === 4 && data.statusCode === 0) {
-        alert('Transaction successful');
-      } else {
-        alert('Transaction Failed! Check console for more details');
-        console.log('transaction error: ' + data.errorMessage);
-      }
-      setTransactionLoading(false);
-      setToAddress('');
-      setAmount('');
-    } catch (e) {
-      setTransactionLoading(false);
-      alert('Transaction Failed! Check console for more details');
-      console.log(e);
-    }
-  }, [magic, amount, publicAddress, toAddress]);
+			if (data.status === 4 && data.statusCode === 0) {
+				alert('Transaction successful');
+			} else {
+				alert('Transaction Failed! Check console for more details');
+				console.log('transaction error: ' + data.errorMessage);
+			}
+			setTransactionLoading(false);
+			setToAddress('');
+			setAmount('');
+		} catch (e) {
+			setTransactionLoading(false);
+			alert('Transaction Failed! Check console for more details');
+			console.log(e);
+		}
+	}, [magic, amount, publicAddress, toAddress]);
 
-  return (
-    <Card>
-      <CardHeader id="send-transaction">Send Transaction</CardHeader>
-      {getNetwork() == 'testnet' && (
-        <div>
-          <a href={getFaucetUrl()} target="_blank" rel="noreferrer">
-            <FormButton onClick={() => null} disabled={false}>
-              Get Test FLOW
-              <Image src={Link} alt="link-icon" className="ml-[3px]" />
-            </FormButton>
-          </a>
-          <Divider />
-        </div>
-      )}
+	return (
+		<Card>
+			<CardHeader id="send-transaction">Send Transaction</CardHeader>
+			{getNetwork() == 'testnet' && (
+				<div>
+					<a href={getFaucetUrl()} target="_blank" rel="noreferrer">
+						<FormButton onClick={() => null} disabled={false}>
+							Get Test FLOW
+							<Image
+								src={Link}
+								alt="link-icon"
+								className="ml-[3px]"
+							/>
+						</FormButton>
+					</a>
+					<Divider />
+				</div>
+			)}
 
-      <FormInput
-        value={toAddress}
-        onChange={(e: any) => setToAddress(e.target.value)}
-        placeholder="Receiving Address"
-      />
-      {toAddressError ? <ErrorText>Invalid address</ErrorText> : null}
-      <FormInput value={amount} onChange={(e: any) => setAmount(e.target.value)} placeholder={`Amount (FLOW)`} />
-      {amountError ? <ErrorText className="error">Invalid amount</ErrorText> : null}
-      <FormButton onClick={sendTransaction} disabled={!toAddress || !amount || disabled}>
-        {transactionLoading ? (
-          <div className="w-full loading-container">
-            <Spinner />
-          </div>
-        ) : (
-          'Send Transaction'
-        )}
-      </FormButton>
+			<FormInput
+				value={toAddress}
+				onChange={(e: any) => setToAddress(e.target.value)}
+				placeholder="Receiving Address"
+			/>
+			{toAddressError ? <ErrorText>Invalid address</ErrorText> : null}
+			<FormInput
+				value={amount}
+				onChange={(e: any) => setAmount(e.target.value)}
+				placeholder={`Amount (FLOW)`}
+			/>
+			{amountError ? (
+				<ErrorText className="error">Invalid amount</ErrorText>
+			) : null}
+			<FormButton
+				onClick={sendTransaction}
+				disabled={!toAddress || !amount || disabled}
+			>
+				{transactionLoading ? (
+					<div className="w-full loading-container">
+						<Spinner />
+					</div>
+				) : (
+					'Send Transaction'
+				)}
+			</FormButton>
 
-      {hash ? (
-        <>
-          <Spacer size={20} />
-          <TransactionHistory />
-        </>
-      ) : null}
-    </Card>
-  );
+			{hash ? (
+				<>
+					<Spacer size={20} />
+					<TransactionHistory />
+				</>
+			) : null}
+		</Card>
+	);
 };
 
 export default SendTransaction;
